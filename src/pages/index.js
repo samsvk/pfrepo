8f52cd76be05128b52026c1b9fe8627442eef4ef
samsvk <samsvk00@gmail.com>
2022-11-12 19:04:39 +0000

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,77 +23,91 @@ export default function Home() {
           }}
         >
           <div className="container__face__inner">
-            <main className="principale">
-              <div className="globale">
-                <div className="testo">
-                  <p>
-                    <label className="lista__titolo">About</label>
-                    Samuel graduated in digital design & development, Samuel is a
-                    passionate engineer specializing in interaction, animation, and
-                    motion. Focusing on interactive user interface with rich visuals
-                    and best practice in accessibility with scalable modern
-                    technology.
-                  </p>
+            <div className="involucro">
+              <section className="superiore">
+                <h1 className="superiore__titolo">SAM CAMPBELL (23) IS A</h1>
+                <h1 className="superiore__titolo">*CREATIVE DEVELOPER*</h1>
+                <h1 className="superiore__titolo">FROM »SCOTLAND«</h1>
+                <h1 className="superiore__titolo">BASED IN BERGAMO, ITALIA.</h1>
+              </section>
+              <main className="principale">
+                <div className="globale">
+                  <div className="testo">
+                    <p>
+                      <label className="lista__titolo">About</label>
+                      Samuel graduated in digital design & development, Samuel is a
+                      passionate engineer specializing in interaction, animation, and
+                      motion. Focusing on interactive user interface with rich
+                      visuals and best practice in accessibility with scalable modern
+                      technology.
+                    </p>
 
-                  <div>
-                    <label className="lista__titolo">Recent Works</label>
-                    <ul className="progettolista">
-                      {DATA.map((project, index) => (
-                        <div className="progettolista__elemento">
-                          <h5 className="progettolista__elemento__titolo">
-                            {project.name}
-                            <RiArrowRightDownLine />
-                          </h5>
-                          <ul className="progettolista__elemento__lista">
-                            {project.tech
-                              .slice(0, 2)
-                              .map((item) => item)
-                              .join(", ")}
-                          </ul>
-                        </div>
-                      ))}
-                    </ul>
+                    <div>
+                      <label className="lista__titolo">Recent Works</label>
+                      <ul className="progettolista">
+                        {DATA.map((project, index) => (
+                          <div key={index} className="progettolista__elemento">
+                            <h5 className="progettolista__elemento__titolo">
+                              {project.name}
+                              <RiArrowRightDownLine />
+                            </h5>
+                            <ul className="progettolista__elemento__lista">
+                              {project.tech
+                                .slice(0, 2)
+                                .map((item) => item)
+                                .join(", ")}
+                            </ul>
+                          </div>
+                        ))}
+                      </ul>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="globale">
-                <ul className="lista">
-                  <label className="lista__titolo">Who</label>
-                  <li className="lista__elemento">Samuel Campbell</li>
-                </ul>
-                <ul className="lista">
-                  <label className="lista__titolo">Location</label>
-                  <li className="lista__elemento">Bergamo, Italia</li>
-                  <li className="lista__elemento">45.6983° N, 9.6773° E</li>
-                </ul>
-                <ul className="lista">
-                  <label className="lista__titolo">Email</label>
-                  <li className="lista__elemento">hello@samsvk.com</li>
-                </ul>
-                <ul className="lista">
-                  <label className="lista__titolo">Socials</label>
-                  <li className="lista__elemento lista__elemento--link">Github</li>
-                  <li className="lista__elemento lista__elemento--link">Twitter</li>
-                  <li className="lista__elemento lista__elemento--link">Dribbble</li>
-                  <li className="lista__elemento lista__elemento--link">Spotify</li>
-                </ul>
-              </div>
-              <div className="globale">
-                <ul className="lista">
-                  <label className="lista__titolo">Currently at</label>
-                  <li className="lista__elemento">Fulltime — Freelance</li>
-                </ul>
+                <div className="globale">
+                  <ul className="lista">
+                    <label className="lista__titolo">Who</label>
+                    <li className="lista__elemento">Samuel Campbell</li>
+                  </ul>
+                  <ul className="lista">
+                    <label className="lista__titolo">Location</label>
+                    <li className="lista__elemento">Bergamo, Italia</li>
+                    <li className="lista__elemento">45.6983° N, 9.6773° E</li>
+                  </ul>
+                  <ul className="lista">
+                    <label className="lista__titolo">Email</label>
+                    <li className="lista__elemento">hello@samsvk.com</li>
+                  </ul>
+                  <ul className="lista">
+                    <label className="lista__titolo">Socials</label>
+                    <li className="lista__elemento lista__elemento--link">Github</li>
+                    <li className="lista__elemento lista__elemento--link">
+                      Twitter
+                    </li>
+                    <li className="lista__elemento lista__elemento--link">
+                      Dribbble
+                    </li>
+                    <li className="lista__elemento lista__elemento--link">
+                      Spotify
+                    </li>
+                  </ul>
+                </div>
+                <div className="globale">
+                  <ul className="lista">
+                    <label className="lista__titolo">Currently at</label>
+                    <li className="lista__elemento">Fulltime — Freelance</li>
+                  </ul>
 
-                <ul className="lista">
-                  <label className="lista__titolo">
-                    Available for freelance
-                    <br />
-                    (creative) work from:
-                  </label>
-                  <li className="lista__elemento">January 2023</li>
-                </ul>
-              </div>
-            </main>
+                  <ul className="lista">
+                    <label className="lista__titolo">
+                      Available for freelance
+                      <br />
+                      (creative) work from:
+                    </label>
+                    <li className="lista__elemento">January 2023</li>
+                  </ul>
+                </div>
+              </main>
+            </div>
           </div>
         </div>
 
