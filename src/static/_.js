bd5fecf800f6f155722fae4a554844a7de668226
samsvk <samsvk00@gmail.com>
2022-11-15 16:17:32 +0000

    fix

--- src/static/_.js
+++ src/static/_.js
@@ -14,9 +14,9 @@ export default [
     scope: ["Active", "4 Weeks"],
     brief: `Viper is an online educational platform providing outstanding courses across various gaming titles. Providing users with unlimited guides through a one-time payment model Viperino aims to provide high-quality guides with a highly functional interactive web app. My role in Viperino was to understand the groups' vision for Viperino's visual identity and bridge that gap by creating a full-scale web app that reflects that identity using modern web technologies`,
     imgs: [
-      "/projects/viperino/main.png",
-      "/projects/viperino/main2.png",
-      "/projects/viperino/main3.png",
+      "/projects/viperino/main.jpg",
+      "/projects/viperino/main2.jpg",
+      "/projects/viperino/main3.jpg",
     ],
     strategy: `I began researching competitors and highly consider their brand strategies. After numerous references to other educational sites and internal conversations, it became apparent Viperino required a design that emphasized guide content with an easy-to-use navigation and filtering system.`,
     tech: ["Next", "Stripe", "Tailwind"],
@@ -32,9 +32,9 @@ quickly and subsequently serving appropriate results with as minimal steps as po
 success. By creating a simple wirefream and webflow, I was able to translate that directly into the application.`,
     tech: ["Typescript", "SASS", "Node", "Socket.io"],
     imgs: [
-      "/projects/nextflix/main.png",
-      "/projects/nextflix/main3.png",
-      "/projects/nextflix/main4.png",
+      "/projects/nextflix/main.jpg",
+      "/projects/nextflix/main3.jpg",
+      "/projects/nextflix/main4.jpg",
     ],
   },
   {
@@ -42,7 +42,7 @@ success. By creating a simple wirefream and webflow, I was able to translate tha
     desc: `Front-end studio display to display their latest art with an interactive fluid-grid.`,
     scope: ["Active", "5 Weeks"],
     brief: `Vivi is a digital artist creating unique designs for leading industry streamers on all social medias. As Vivi became more successful, she realised it was important to have a centralised location to display her art and also have a terms of service that users have to agree to before being able to contact her.`,
-    imgs: ["/projects/vi/main.png", "/projects/vi/main2.png"],
+    imgs: ["/projects/vi/main.jpg", "/projects/vi/main2.jpg"],
     strategy: `I understood that I wanted to have large bold design using imagery to catch the user's eye. Using a fluid-grid layout I was able
     to present each image in its own unique way and relative to its aspect ratio. Using Puppeteer, I was able to web-scrape Vivi's Twitch Channel to display her commission status.`,
     tech: ["Next", "Puppeteer", "Tailwind"],
@@ -67,7 +67,7 @@ success. By creating a simple wirefream and webflow, I was able to translate tha
     strategy:
       "Taking into consideration Warcraft's RPG value and Blizzard Ent's unique design patterns I felt it was necessary to emulate that fantasy feeling.",
     tech: ["Typescript", "BEM"],
-    imgs: ["/projects/mograine/main.png", "/projects/mograine/main2.png"],
+    imgs: ["/projects/mograine/main.jpg", "/projects/mograine/main2.jpg"],
   },
   {
     name: "Pigfingers",
@@ -78,6 +78,6 @@ success. By creating a simple wirefream and webflow, I was able to translate tha
     strategy:
       "Typing has become a modern aesthetic - building a visually appealing site to match that was the baseline for Pigfingers. By using a mono font and vscode theming we were able to quickly achieve an eye-candy aesthetic that translated throughout the entire Pigfingers site.",
     tech: ["React", "SASS"],
-    imgs: ["/projects/pigfingers/main.png", "/projects/pigfingers/main2.png"],
+    imgs: ["/projects/pigfingers/main.jpg", "/projects/pigfingers/main2.jpg"],
   },
 ];
